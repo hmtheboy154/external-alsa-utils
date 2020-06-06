7bcffe4056127dc5b8de4004b2e45da876e8ddd4
hmtheboy154 <buingoc67@gmail.com>
2020-06-06 17:27:44 +0700

    Adding alsa_loop and alsa_alsamixer

--- android/Android.mk
+++ android/Android.mk
@@ -34,6 +34,13 @@ ALSA_UTILS_C_INCLUDES := \
 	$(LOCAL_PATH)include \
 	$(LOCAL_PATH)android
 
+NCURSES_INCLUDE := \
+	$(LOCAL_PATH)/../libncurses/ncurses \
+	$(LOCAL_PATH)/../libncurses/panel \
+	$(LOCAL_PATH)/../libncurses/include \
+	$(LOCAL_PATH)/../libncurses/form \
+	$(LOCAL_PATH)/../libncurses/menu
+	
 # Build amixer command
 include $(CLEAR_VARS)
 
@@ -101,5 +108,52 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE := alsa_ucm
 LOCAL_SHARED_LIBRARIES := libasound
 LOCAL_ADDITIONAL_DEPENDENCIES := $(GEN)
+
+include $(BUILD_EXECUTABLE)
+
+# Build alsaloop command
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := $(ALSA_UTILS_CFLAGS)
+LOCAL_C_INCLUDES:= $(ALSA_UTILS_C_INCLUDES)
+
+LOCAL_SRC_FILES := \
+        alsaloop/alsaloop.c \
+        alsaloop/control.c \
+        alsaloop/pcmjob.c
+
+LOCAL_MODULE := alsa_loop
+LOCAL_SHARED_LIBRARIES := libasound
+LOCAL_ADDITIONAL_DEPENDENCIES := $(GEN)
+
+include $(BUILD_EXECUTABLE)
+
+# Build alsamixer command
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := $(ALSA_UTILS_CFLAGS) -D_GNU_SOURCE -lcurses -lmenu -lpanel -lncurses -lncursesw
+LOCAL_C_INCLUDES:= $(ALSA_UTILS_C_INCLUDES) $(NCURSES_INCLUDE) 
+LOCAL_SRC_FILES := \
+		alsamixer/card_select.c \
+		alsamixer/cli.c \
+		alsamixer/colors.c \
+		alsamixer/device_name.c \
+		alsamixer/die.c \
+		alsamixer/mainloop.c \
+		alsamixer/mem.c \
+		alsamixer/mixer_controls.c \
+		alsamixer/mixer_display.c \
+		alsamixer/mixer_widget.c \
+		alsamixer/proc_files.c \
+		alsamixer/textbox.c \
+		alsamixer/utils.c \
+		alsamixer/volume_mapping.c \
+		alsamixer/widget.c
+
+LOCAL_MODULE := alsa_alsamixer
+LOCAL_SHARED_LIBRARIES := libasound libncurses libmenu libpanel libform
+LOCAL_ADDITIONAL_DEPENDENCIES := $(GEN)
+man_MANS = alsamixer.1
+EXTRA_DIST = alsamixer.1
 
 include $(BUILD_EXECUTABLE)
